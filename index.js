79089972816172f74e89f733b33a9572689ead9a
roshan <102507924+roshanpanda666@users.noreply.github.com>
2023-01-26 17:30:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,30 +10,7 @@ document.querySelector("button").addEventListener("click",clicked)
 function clicked(){
 
 
-if(inp2==String()){
-    document.getElementById("number").style.borderColor="red"
-    document.getElementById("cardholdernumber").innerHTML='enter the correct number'
-    document.getElementById("cardholdernumber").style.color="red"
-}
 
-if(inp3==String()){
-    document.getElementById("ddmmyy").style.borderColor="red"
-    document.getElementById("lableddmmyy").innerHTML='enter the month in number'
-    
-}
-
-if(inp4==String()){
-    document.getElementById("ddmmyy2").style.borderColor="red"
-    document.getElementById("lableddmmyy").innerHTML='enter the year in number'
-    
-}
-
-if(inp5==String()){
-    document.getElementById("cvc").style.borderColor="red"
-    document.getElementById("lableddmmyy").innerHTML='enter the correct cvc number'
-    
-}
-else{
 document.getElementById("namef").innerHTML=inp1
 document.getElementById("numberf").innerHTML=inp2
 document.getElementById("mon").innerHTML=inp3
@@ -42,5 +19,4 @@ document.getElementById("cvcf").innerHTML=inp5
 document.getElementById("inputs").style.display="none"
 document.getElementById("thanks").style.display="block"
 
-}
 }
